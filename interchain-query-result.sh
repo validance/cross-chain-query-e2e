f16f8a24a5b6a5fefafd665a2ec35f5781f4ddc8
SangyunOck <demibun123@naver.com>
2022-10-06 04:29:37 +0900

    feat: validator key, execute direct from container

--- interchain-query-result.sh
+++ interchain-query-result.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-interchain-queriesd query ibc_query query-result query-0 --chain-id ibc-0 --node http://localhost:27010
-interchain-queriesd query ibc_query query-result query-1 --chain-id ibc-0 --node http://localhost:27010
+docker exec blockchains interchain-queriesd query ibc_query query-result query-0 --chain-id ibc-0 --node http://localhost:27010
+docker exec blockchains interchain-queriesd query ibc_query query-result query-1 --chain-id ibc-0 --node http://localhost:27010
